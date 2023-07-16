08e5f240c29037c8b6dacddffe4bdeb8a3a468a5
ayaka-ikeda-A <ayaka.makomako0711@icloud.com>
2023-07-16 09:56:59 +0900

    Lesson_Contactform

---
+++ insert.php
@@ -0,0 +1,33 @@
+<?php
+    
+    mb_internal_encoding("utf8");
+
+    $pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","");
+
+    $pdo->exec("insert into contactform(name, mail, age, comments)
+    value('".$_POST['name']."','".$_POST['mail']."','".$_POST['age']."','".$_POST['comments']."');");
+
+?>
+
+<!DOCTYPE html>
+<html lang=“ja”>
+
+<head>
+    <meta charset = "UTF-8">
+    <title>お問い合わせフォーム</title>
+    <link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+
+<body>
+
+    <h1>お問い合わせフォーム</h1>
+
+    <div class = "confirm">
+        <p>
+            お問い合わせ有難うございました。<br>３営業日以内に担当よりご連絡さしあげます。
+        </p>
+    </div>
+
+</body>
+
+</html>
